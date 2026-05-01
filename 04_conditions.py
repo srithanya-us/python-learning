abffc9796b5b95db238cb3863e4d0559c2e84e17
Sri Thanya <srithanya.us03@gmail.com>
2026-05-01 23:23:39 +0530

    Create 04_conditions.py

---
+++ 04_conditions.py
@@ -0,0 +1,19 @@
+# Check if a number is positive, negative, or zero
+
+num = 10
+
+if num > 0:
+    print("Positive number")
+elif num < 0:
+    print("Negative number")
+else:
+    print("Zero")
+
+# Check eligibility
+
+age = 22
+
+if age >= 18:
+    print("Eligible to vote")
+else:
+    print("Not eligible")
