02df3941fa82b9e8c77bce57ce8c3fd734ff0483
Sri Thanya <srithanya.us03@gmail.com>
2026-05-01 23:18:51 +0530

    Create 02_functions.py

---
+++ 02_functions.py
@@ -0,0 +1,6 @@
+def display_profile(name, role, experience):
+    print("Name:", name)
+    print("Role:", role)
+    print("Experience:", experience, "years")
+
+display_profile("Sri Thanya", "Aspiring Data Analyst", 2.6)
